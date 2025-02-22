fa806809a424bc6d284d900db5d32b14e6ca6a18
Monarch Corps <monarchcorps01@gmail.com>
2025-02-22 10:55:06 +0100

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    'https://neo-portal-frontend.vercel.app',
+    'https://neoportal.vercel.app',
     'https://neoportal-backend.onrender.com'
 ]
 module.exports = allowedOrigins
